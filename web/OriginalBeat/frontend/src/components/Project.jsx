d8938f800cfad7517d2c36b9a0865f955e66ce96
Christian Quintero <cquintero@scu.edu>
2019-05-03 10:23:13 -0700

    resync for project page

--- web/OriginalBeat/frontend/src/components/Project.jsx
+++ web/OriginalBeat/frontend/src/components/Project.jsx
@@ -18,6 +18,8 @@ class Project extends React.Component{
 	playAll() {
 		var iframe = document.getElementById('melody');
 		var innerDoc1 = iframe.contentDocument || iframe.contentWindow.document;
+		console.log(innerDoc1.server);
+		console.log("innerdoc1");
 		var iframe = document.getElementById('harmony');
 		var innerDoc2 = iframe.contentDocument || iframe.contentWindow.document;
 		var iframe = document.getElementById('drum');
@@ -28,6 +30,9 @@ class Project extends React.Component{
 		innerDoc3.getElementById('PlayPause').click();
 
      }
+	 reSync(){
+		 window.location.reload();
+	 }
 
 	render() {
 		return (
@@ -38,53 +43,64 @@ class Project extends React.Component{
 						<Col>
 
 								<Button color="secondary" onClick={this.playAll}>Play All</Button>
+								<Button color="secondary" onClick={this.reSync}>Re-Sync</Button>
 
 						</Col>
 					</Row>
 
 					<Row>
-						<Col xs="4" sm="2" ></Col>
+						<Col xs="4" sm="2" >
+							<h3 Style="font-size: 30px;">Original Melody</h3>
+						</Col>
 				    	<Col xs="8" sm="8" >
 
-								<h3>Original Melody</h3>
+
 
 								<MelodyRoll/>
-								<Form method="get" action="/download/">
-								<Button id="ddd" type="submit" color="primary">Download</Button>
-								</Form>
 
 						</Col>
+
 				  		<Col sm="2">
-					</Col>
+							<Form method="get" action="/download/">
+								<Button type="submit" color="primary">Download</Button>
+							</Form>
+						</Col>
 
 					</Row>
 
 					<Row>
-						<Col xs="4" sm="2" ></Col>
+						<Col xs="4" sm="2" >
+							<h3 Style="font-size: 30px;">Harmony Layer</h3>
+						</Col>
 						<Col xs="8" sm="8" >
 
-								<h3>Melody with Harmonizing Layer</h3>
+
 								<HarmonyRoll/>
-								<p>
-								  <Button color="primary">Download</Button>
-								</p>
+
 
 						</Col>
-						<Col sm="2"></Col>
+						<Col sm="2">
+							<p>
+							  <Button color="primary">Download</Button>
+							</p>
+						</Col>
 					</Row>
 
 					<Row>
-						<Col xs="4" sm="2" ></Col>
+						<Col xs="4" sm="2" >
+							<h3 Style="font-size: 30px;">Drums Layer</h3>
+						</Col>
 						<Col xs="8" sm="8" >
 
-								<h3>Melody with Harmonizing Layer and Drums</h3>
+
 								<DrumRoll/>
-								<p>
-								  <Button color="primary">Download</Button>
-								</p>
 
 						</Col>
-						<Col sm="2"></Col>
+						<Col sm="2">
+							<p>
+							  <Button color="primary">Download</Button>
+							</p>
+						</Col>
 					</Row>
 				</div>
 				<Footer/>
