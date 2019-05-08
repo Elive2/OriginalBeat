25d92d13f3f159e090b1f427a558cb51425eb8c3
Christian Quintero <cquintero@scu.edu>
2019-05-07 20:31:55 -0700

    Setting up for data pass btwn container to iframe

--- web/OriginalBeat/frontend/src/components/MelodyRoll.jsx
+++ web/OriginalBeat/frontend/src/components/MelodyRoll.jsx
@@ -2,17 +2,29 @@ import React from 'react';
 import '../index.css';
 import { Container, Row, Col, Form, FormGroup, Label, Input, FormText, Jumbotron, Button} from 'reactstrap';
 
+
 class MelodyRoll extends React.Component{
 	constructor(props) {
 	  super(props);
 	}
+
+	setUpFrame() {
+		var frame = window.frames['melody'];
+		frame.setMidiPath('../data/output/output_melody.mid');
+	}
+
 	render() {
+
+
 		return (
 			<div>
 				<Row>
 					<Col>
 						<div className="pianorollFrame">
-							<iframe id = "melody" src={'/static/pianoRoll.html'} scrolling="no" height="300" width="100%" ></iframe>
+
+							<body>
+								<iframe name = "melody" id = "melody" src={'/static/pianoRoll.html'} scrolling="no" height="300" width="100%" ></iframe>
+							</body>
 						</div>
 					</Col>
 				</Row>
